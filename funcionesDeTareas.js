f66370d9cb10fa96b42afe152446369099fc96b9
javilaiton <laiton2395@gmail.com>
2021-09-19 19:41:31 -0500

    Se crean las funciones requeridas para el caso listar y para el caso crear

--- funcionesDeTareas.js
+++ funcionesDeTareas.js
@@ -1,23 +1,30 @@
-
+const fs = require("fs")
 const tareas = require("./tareas.json")
 
-
-let ejecutarFunciones = (accion) => {
-    switch (accion) {
-        
-        case "listar":
-            console.log(tareas)
-            break;
-    
-        case undefined:
-            console.log("Atención - Tienes que pasar una acción.")
-            break;
-    
-        default:
-            console.log("No entiendo qué quieres hacer.");
-            break;
-    }
+const listar = (item, index) => {   // Función que entra como callback en el foreach para mostrar la lista de tareas existentes.
+    console.log(item=tareas[index])
 }
 
-module.exports = ejecutarFunciones
+const crearTarea= title => {    // Función principal que crea, agrega y guarda una nueva tarea en el array de lista de tareas JSON.
+    let nuevaTarea = {
+        titulo : title,
+        estado : "Pendiente"
+    }
+    guardarTarea(nuevaTarea)
+}
+
+const guardarTarea = (nuevaTarea) => {  // Función encargada de convertir lista de tareas JSON a fotmato array de objetos literales y agregar la nueva tarea a dicho array de objetos literales.
+    const listTareasJs = tareas
+    listTareasJs.push(nuevaTarea)
+    escribirJSON(listTareasJs)
+}
+
+const escribirJSON = (listTareasJs) =>{  // Función encargada de convertir el array de objetos literales a formato JSON y de Escribirlo en el archivo "tareas.json"
+    const listTareasJSON=  JSON.stringify(listTareasJs,null,4)
+    fs.writeFileSync("./tareas.json",listTareasJSON); 
+}
+
+
+
+module.exports = {crearTarea, listar}
 
