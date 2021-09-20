583d40113914fb88d9adb772b4d81c16a6838fd8
javilaiton <laiton2395@gmail.com>
2021-09-19 19:38:37 -0500

    Se agrega el caso crear y se le aplica el foreach al caso listar

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-//const fs = require ("fs")
-//const tareas = require("./tareas.json")
+const tareas = require("./tareas.json")
 const funciones = require("./funcionesDeTareas.js")
 
 let accion = process.argv[2]
 
-funciones(accion)
+let titulo = process.argv[3]
+
+
+switch (accion) {
+    case "listar":
+        tareas.forEach(funciones.listar);
+        
+        break;
+
+    case "crear":
+        funciones.crearTarea(titulo)
+
+        break;
+
+    case undefined:
+        console.log("Atención - Tienes que pasar una acción.")
+        break;
+
+    default:
+        console.log("No entiendo qué quieres hacer.");
+        break;
+}
+
+
 
